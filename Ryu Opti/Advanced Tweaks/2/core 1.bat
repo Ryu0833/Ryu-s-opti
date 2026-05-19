897d6b099419c034d62321e954cb8fbe719a2c2d
Ryu0833 <169081041+Ryu0833@users.noreply.github.com>
2026-05-19 01:24:47 +0100

    Add files via upload

---
+++ Ryu Opti/Advanced Tweaks/2/core 1.bat
@@ -0,0 +1,21 @@
+@echo off
+call :IsAdmin
+
+powercfg -setacvalueindex SCHEME_CURRENT 48672f38-7a9a-4bb2-8bf8-3d85be19de4e 2bfc24f9-5ea2-4801-8213-3dbae01aa39d 6
+
+goto exit
+
+:exit
+cls
+
+start https://discord.gg/gQjfXSECrj
+start https://www.twitch.tv/ryu0833
+start https://www.tiktok.com/@ryu_val_
+start https://www.tiktok.com/@ryutech0
+
+start cmd /k "echo Adrob tala 3la TikTok w matnssach follow & pause & exit"
+
+
+pause
+endlocal
+exit /b 0
